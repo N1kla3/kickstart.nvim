fb76ee202e4028f408166bb7fa860f8d37de1676
kolya <kolya.vladimirsky@gmail.com>
2024-09-08 13:01:14 +0300

    python support

--- init.lua
+++ init.lua
@@ -583,7 +583,7 @@ require('lazy').setup({
           filetypes = { 'cmake', 'CMakeLists.txt' },
         },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -670,7 +670,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
